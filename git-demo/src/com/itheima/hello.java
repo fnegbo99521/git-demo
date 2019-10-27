af0e01ecca13bdd866b643856842c673f3ff0f80
root <17629038615@163.com>
2019-10-27 19:14:28 +0800

    新建一个Hello

---
+++ git-demo/src/com/itheima/hello.java
@@ -0,0 +1,9 @@
+package com.itheima;
+
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("hello world  !!");
+        System.out.println("hello world  !!");
+        System.out.println("hello world  !!");
+    }
+}
